c186bf9ec40d7330996ced8ea461969a878ae4fa
antoniehuang <antonieyuchi@gmail.com>
2022-08-04 21:35:38 +0100

    Challenge 1 - 04/08/2022

---
+++ 04-AUG-2022/challenge1.py
@@ -0,0 +1,19 @@
+# CW
+
+'''
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?
+
+Examples: 
+input --> output
+
+123 --> "123"
+999 --> "999"
+-100 --> "-100"
+
+'''
+def number_to_string(num):
+    return str(num)
+    # Return a string of the number here!
+    
+# Python doesn't have a toString method, instead we use str() to type cast the object we want to convert to string.
